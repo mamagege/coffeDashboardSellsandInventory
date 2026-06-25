0da7c76458ce633f5ee60749b8551e846d751e49
Juan Diego Gaitan <thedrup321@gmail.com>
2026-06-25 10:46:04 -0500

    Despliegue Render y Neon Tech

--- backend-api/src/main/java/com/cafeNasun/backend_api/controllers/ProductoController.java
+++ backend-api/src/main/java/com/cafeNasun/backend_api/controllers/ProductoController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/productos")
-@CrossOrigin(origins = "http://localhost:5173") // Permite que tu React (Vite) se conecte sin bloqueos de seguridad
+@CrossOrigin(origins = "https://cafe-dashboard-eight.vercel.app") // Permite que tu React (Vite) se conecte sin bloqueos de seguridad
 public class ProductoController {
 
     @Autowired
--- backend-api/src/main/java/com/cafeNasun/backend_api/controllers/VentaController.java
+++ backend-api/src/main/java/com/cafeNasun/backend_api/controllers/VentaController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/ventas")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://cafe-dashboard-eight.vercel.app")
 public class VentaController {
 
     @Autowired
